bde7a21af51020efea931db5675926705a723c82
zotel1 <zotelsigel@gmail.com>
2025-02-17 21:14:24 -0300

    Initial commit

---
+++ src/DeclaracionVariables.java
@@ -0,0 +1,25 @@
+public class DeclaracionVariables {
+
+    public static void main(String[] args) {
+        /*
+        * datos primitivos:
+        * int = enteros,
+        * doubles = numeros con parte decimal,
+        * char = que es un caracter 'a',
+        * long = numero grande 8 bits,
+        * boolean = verdadero o falso,
+        *
+        * String = son considerados una "clase", todo lo que se escriba con comilla dobe es un string,
+        * */
+
+        int numero1; // Declarando una variable
+
+        numero1 = 5; // Asignando o inicializando un variable
+
+        System.out.println("El valor de la variable numero1 es: " + numero1);
+
+        numero1 = 8;
+
+        System.out.println("El nuevo valor de la variable numero1 es: " + numero1);
+    }
+}
